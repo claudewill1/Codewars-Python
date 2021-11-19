c2981a3ce1c001746c96125993e925b7f9d99ffe
Claude Will <claude.will@yahoo.com>
2021-11-18 22:46:49 -0600

    another challenge down

---
+++ 7Kyu/greatestCommonDivisor.py
@@ -0,0 +1,22 @@
+"""
+Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+Algorithms
+Optimization
+Fundamentals
+Recursion
+Computability Theory
+Theoretical Computer Science
+"""
+
+def mygcd(x, y):
+    if x == 0 or x == y:
+        return y
+    if y == 0:
+        return x
+    if x == y:
+        return y
+    if x > y:
+        return mygcd(x-y, y) 
+    return mygcd(x,y-x)
